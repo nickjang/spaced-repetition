f839267145010cf381f173d6161d9ef4aab802fc
nickjang <nickjang@alumni.upenn.edu>
2021-01-17 08:16:03 -0500

    Add back deployed api base url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
 export default {
-  REACT_APP_API_BASE: 'http://localhost:8000/api',
+  REACT_APP_API_BASE: process.env.REACT_APP_API_BASE || 'http://localhost:8000/api',
   TOKEN_KEY: 'client-auth-token',
 }
-
-//process.env.REACT_APP_API_BASE ||
